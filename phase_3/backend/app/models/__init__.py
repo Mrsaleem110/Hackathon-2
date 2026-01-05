224a3f48731f7da80e6eb8dcf8528553cf53aa7d
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2026-01-05 20:38:41 +0300

    updated

--- phase_3/backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import all models to ensure they are registered with SQLModel in the right order
-from .user import User
-from .item import Item
-from .session import Session
-from .task import Task
-from .conversation import Conversation, Message
-
-__all__ = ["User", "Item", "Session", "Task", "Conversation", "Message"]
